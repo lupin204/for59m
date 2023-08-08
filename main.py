cc2dfe3fe9ad7708d834cc3858002d083339cbad
Lupin's Repository <lupin204@gmail.com>
2023-08-09 08:03:09 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+
+
+
+
+options = webdriver.ChromeOptions()
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+options.add_argument('ignore-certificate-errors')
+options.add_argument('user-agent=' + user_agent)
+options.add_argument("lang=ko_KR")
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_argument("--no-sandbox")
+
+# 사람처럼 보이게 하는 옵션들
+options.add_argument("disable-gpu")   # 가속 사용 x
+options.add_argument("lang=ko_KR")    # 가짜 플러그인 탑재
+options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')  # user-agent 이름 설정
+
+# 드라이버 위치 경로 입력
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get('http://www.example.com')
+driver.implicitly_wait(3)
+driver.get_screenshot_as_file('capture_naver.png')    # 화면캡처
+
+driver.quit() # driver 종료
